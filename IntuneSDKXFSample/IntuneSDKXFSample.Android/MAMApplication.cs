7ef46bfcb38f90d0a5b0f2a422f5fc427029bb68
Romuald Zdebskiy <zdebskis@mail.ru>
2018-05-30 18:51:35 +0300

    Add project files.

---
+++ IntuneSDKXFSample/IntuneSDKXFSample.Android/MAMApplication.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Microsoft.Intune.Mam.Client.App;
+
+namespace IntuneSDKXFSample.Droid
+{
+    [Application]
+    public class IntuneMAMFormsSampleApplication : MAMApplication
+    {
+        /// <summary>
+        /// This is necessary because of a leaky abstraction somewhere up the chain:
+        /// http://stackoverflow.com/questions/10593022/monodroid-error-when-calling-constructor-of-custom-view-twodscrollview/10603714#10603714
+        /// </summary>
+        /// <param name="handle">Java reference</param>
+        /// <param name="transfer">Ownership transfer</param>
+        public IntuneMAMFormsSampleApplication(IntPtr handle, JniHandleOwnership transfer)
+            : base(handle, transfer)
+        {
+        }
+
+        /// <summary>
+        /// New abstract method that we need to override
+        /// Documentation at https://microsoft.sharepoint.com/teams/Android_SSP/_layouts/15/WopiFrame2.aspx?sourcedoc=%7b56C60010-40D5-4487-BC70-21471C50D1DD%7d&file=Walled%20Garden%20API%20Guide.docx&action=default says:
+        /// If your application does not call AuthenticationSettings.setSecretKey (or does not integrate ADAL at all), you may simply return null. 
+        /// </summary>
+        /// <returns>The ADAL key</returns>
+        public override byte[] GetADALSecretKey()
+        {
+            return null;
+        }
+    }
+}
---
+++ IntuneSDKXFSample/IntuneSDKXFSample.Android/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Views;
+using Android.OS;
+using Android.Content;
+using Android.App.Assist;
+
+namespace IntuneSDKXFSample.Droid
+{
+    [Activity(Label = "IntuneSDKXFSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnMAMCreate(Bundle bundle)
+        {
+            base.OnMAMCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new IntuneSDKXFSample.App());
+        }
+
+        protected override void OnMAMActivityResult(int i, Result j, Intent intent)
+        {
+            base.OnMAMActivityResult(i, j, intent);
+        }
+
+        protected override void OnMAMDestroy()
+        {
+            base.OnMAMDestroy();
+        }
+
+        protected override void OnMAMPause()
+        {
+            base.OnMAMPause();
+        }
+
+        protected override void OnMAMResume()
+        {
+            base.OnMAMResume();
+        }
+
+        protected override void OnMAMNewIntent(Intent intent)
+        {
+            base.OnMAMNewIntent(intent);
+        }
+
+        public override void OnMAMPostCreate(Bundle p0)
+        {
+            base.OnMAMPostCreate(p0);
+        }
+
+        public override void OnMAMPostResume()
+        {
+            base.OnMAMPostResume();
+        }
+
+        public override void OnMAMProvideAssistContent(AssistContent p0)
+        {
+
+            base.OnMAMProvideAssistContent(p0);
+        }
+
+        public override void OnMAMSaveInstanceState(Bundle p0)
+        {
+            base.OnMAMSaveInstanceState(p0);
+        }
+
+        public override void OnMAMStateNotSaved()
+        {
+            base.OnMAMStateNotSaved();
+        }
+
+        public override bool OnMAMPrepareOptionsMenu(IMenu p0)
+        {
+            return base.OnMAMPrepareOptionsMenu(p0);
+        }
+
+        public override bool OnMAMSearchRequested(SearchEvent p0)
+        {
+            return base.OnMAMSearchRequested(p0);
+        }
+
+        public override Android.Net.Uri OnMAMProvideReferrer()
+        {
+
+            return base.OnMAMProvideReferrer();
+        }
+    }
+}
+
